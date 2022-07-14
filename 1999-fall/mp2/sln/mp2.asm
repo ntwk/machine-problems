7c2a941715a114688ad1e571e9d54a8257923833
rnndxb wxcy <rnndxb@gmail.com>
2022-07-14 07:31:24 -0400

    Implement / operator in SolveOne procedure

--- 1999-fall/mp2/sln/mp2.asm
+++ 1999-fall/mp2/sln/mp2.asm
@@ -798,6 +798,21 @@ SolveOne proc near
      jmp     SolveOne_return
 
   divide:
+     call    GetOp1
+     jc      SolveOne_return                    ; check for error
+     mov     ax, WORD PTR controlStr[bp+1]
+     mov     cx, 4
+  divide_insertnull:
+     mov     BYTE PTR controlStr[bp], NULL
+     inc     bp
+     loop    divide_insertnull
+     call    GetOp2
+     jc      SolveOne_return                    ; check for error
+     cwd                                        ; sign-extend ax to dx:ax
+     idiv    WORD PTR controlStr[bp+1]
+     mov     WORD PTR controlStr[bp+1], ax      ; store the result
+     jmp     SolveOne_return
+
   shiftleft:
   shiftright:
      jmp     errnosuchop
